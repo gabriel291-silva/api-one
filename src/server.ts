68a6e322ddab2fe8cbec1d955f3528600ea5626a
Gabriel Padilha da Silva <gabriel@crpmango.com.br>
2024-01-27 16:37:01 -0300

    deploy aplication

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import { z } from 'zod'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.get('/users', async () =>{
+    const users = await prisma.user.findMany()
+
+    return { users}
+})
+
+app.post('/users',  async (request,reply)=>{
+    const CreateUserSchema = z.object({
+        name: z.string(),
+        email: z.string().email()
+    })
+    const { name, email} = CreateUserSchema.parse(request.body)
+
+    await prisma.user.create({
+        data:{
+            name,
+            email
+        }
+    })
+
+    return reply.status(201).send({email,name})
+})
+
+
+app.listen({
+    host:"0.0.0.0",
+    port:process.env.PORT ? Number(process.env.PORT) : 3333,
+}).then(  ()=>console.log("http serve online"))
